c1b38964315bced02aa51596b5e074402d3b5033
Dima Tisnek <dimaqq@gmail.com>
2022-01-19 15:37:45 +0900

    cleanup

--- poetry_sample.py
+++ poetry_sample.py
@@ -12,6 +12,5 @@ async def root():
 
 
 def serve():
-    """Launched with `poetry run start` at root level"""
     import uvicorn
     uvicorn.run("poetry_sample:app", host="0.0.0.0", port=8000, reload=True)
